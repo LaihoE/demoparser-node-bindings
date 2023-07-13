1fba7fb6386579611347e449d4577971312e982e
Laiho <emillaiho@hotmail.fi>
2023-07-13 16:20:00 +0300

    0.1.5

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ try {
     const filePath = path.join("/home/laiho/Documents/demos/cs2/test2/", file);
     // let x = parseEvents(filePath, "player_death")
     // let y = parsePlayerInfo(filePath)
-    let y = parseTicks(filePath, ["X"], [10000, 10001, 10002, 10003])
+    let y = parseTicks(filePath, ["X"])
     //let y = listGameEvents(filePath)
     console.log(file)
   });
